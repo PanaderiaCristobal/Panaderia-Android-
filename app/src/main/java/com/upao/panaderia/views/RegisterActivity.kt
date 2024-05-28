689a4edc2fe94aa945c90addab5741d02f963000
Alejandro Escobedo Correa <toshiro.escobedo@intediperu.com>
2024-05-28 01:23:49 -0500

    updates

---
+++ app/src/main/java/com/upao/panaderia/views/RegisterActivity.kt
@@ -0,0 +1,60 @@
+package com.upao.panaderia.views
+
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.SystemBarStyle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.upao.panaderia.R
+import com.upao.panaderia.databinding.ActivityRegisterBinding
+import com.upao.panaderia.register.RegisterClientFragment
+import com.upao.panaderia.register.RegisterSellerFragment
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
+        )
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            loadFragment(RegisterClientFragment())
+        }
+
+        binding.registerClient.setBackgroundResource(R.drawable.button_state_selected)
+        binding.registerSeller.setBackgroundResource(R.drawable.button_state_selected)
+        binding.registerClient.backgroundTintList = ContextCompat.getColorStateList(this, R.color.button)
+        binding.registerSeller.backgroundTintList = ContextCompat.getColorStateList(this, R.color.white)
+        binding.registerClient.setOnClickListener {
+            binding.registerClient.backgroundTintList = ContextCompat.getColorStateList(this, R.color.button)
+            binding.registerSeller.backgroundTintList = ContextCompat.getColorStateList(this, R.color.white)
+            binding.registerClient.setTextAppearance(R.style.ButtonSelected)
+            binding.registerSeller.setTextAppearance(R.style.ButtonNoSelected)
+            loadFragment(RegisterClientFragment())
+        }
+
+        binding.registerSeller.setOnClickListener{
+            binding.registerSeller.backgroundTintList = ContextCompat.getColorStateList(this, R.color.button)
+            binding.registerClient.backgroundTintList = ContextCompat.getColorStateList(this, R.color.white)
+            binding.registerSeller.setTextAppearance(R.style.ButtonSelected)
+            binding.registerClient.setTextAppearance(R.style.ButtonNoSelected)
+            loadFragment(RegisterSellerFragment())
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container, fragment)
+            commit()
+        }
+    }
+}
